4f34d26533c96563cc490eda57ae34332c2a70af
Nurmuhammad <nursherzil@gmail.com>
2026-05-31 18:10:11 +0500

    serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,20 @@
 #include <Arduino.h>
 #include <Servo.h>
-int buttons[] = {2,3,4};
-int size = sizeof(buttons) / sizeof(buttons[0]);
-Servo myServo;
+int pin = 2;
+int value = 0;
 
 void setup() {
-  for(int i = 0; i < size; i++) {
-    pinMode(buttons[i], INPUT_PULLUP);
-  }
-  myServo.attach(12);
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
 }
 
 void loop() {
-  while(digitalRead(2) == LOW) {
-    myServo.write(0); // left
+  value = digitalRead(pin);
+  if (value == HIGH) {
+    Serial.println("On");
+  } else {
+    Serial.println("Off");
   }
-  while(digitalRead(3) == LOW) {
-    myServo.write(90); // bottom
-  }
-  while(digitalRead(4) == LOW) {
-    myServo.write(180);// right
-  }
-  myServo.write(90);
+  delay(1000);
 }
 
