2eeb536557cd800610735cb5f7c6ab71c19b251a
Nurmuhammad <nursherzil@gmail.com>
2026-05-31 21:58:22 +0500

    serial monitor control

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,21 @@
 #include <Arduino.h>
-#include <Servo.h>
-Servo myServo;
-int potPin = A0;
-int servoPin = 2;
-int led = 3;
+int option;
+int led = 13;
+
 void setup() {
   Serial.begin(9600);
-  myServo.attach(servoPin);  
   pinMode(led, OUTPUT);
 }
 
 void loop() {
-  int natija = analogRead(potPin);
-  Serial.println(natija);
-  int burchak = map(natija, 0, 1023, 0, 180);
-  int yoruglik = map(natija, 0, 1023, 0, 255);
-  analogWrite(led, yoruglik);
-  myServo.write(burchak);
-  delay(50);
+  if(Serial.available() > 0) {
+    option = Serial.read();
+    if(option == 'a') {
+      digitalWrite(led, LOW);
+    }
+    if(option == 's') {
+      digitalWrite(led, HIGH);
+    }
+  }
 }
 
