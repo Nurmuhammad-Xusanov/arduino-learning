fcb17e384bc16e37f4dc64e788e29407f5d6a4b5
Nurmuhammad <nursherzil@gmail.com>
2026-05-31 21:13:20 +0500

    patensiometer

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include <Arduino.h>
 #include <Servo.h>
-int pin = 2;
-int value = 0;
-
+Servo myServo;
+int potPin = A0;
+int servoPin = 2;
+int led = 3;
 void setup() {
   Serial.begin(9600);
-  pinMode(pin, INPUT);
+  myServo.attach(servoPin);  
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
-  value = digitalRead(pin);
-  if (value == HIGH) {
-    Serial.println("On");
-  } else {
-    Serial.println("Off");
-  }
-  delay(1000);
+  int natija = analogRead(potPin);
+  Serial.println(natija);
+  int burchak = map(natija, 0, 1023, 0, 180);
+  int yoruglik = map(natija, 0, 1023, 0, 255);
+  analogWrite(led, yoruglik);
+  myServo.write(burchak);
+  delay(50);
 }
 
