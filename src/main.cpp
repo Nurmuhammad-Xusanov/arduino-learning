e8ff24905495c23f0e454ded99ae2eb3751bf354
Nurmuhammad <nursherzil@gmail.com>
2026-06-02 16:44:45 +0500

    serial plotter'

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
 #include <Arduino.h>
-int option;
-int led = 13;
 
-void setup() {
+void setup() { 
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
 }
 
 void loop() {
-  if(Serial.available() > 0) {
-    option = Serial.read();
-    if(option == 'a') {
-      digitalWrite(led, LOW);
-    }
-    if(option == 's') {
-      digitalWrite(led, HIGH);
-    }
-  }
+  float angle = millis() / 1000.0;
+  float value = sin(angle) * 100;
+  Serial.println(value);
+  delay(10);
 }
-
