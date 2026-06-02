e737b8d03373298a2b02e7010403d2eb6789d16e
Nurmuhammad <nursherzil@gmail.com>
2026-06-02 16:57:38 +0500

    plotter with 3 sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 
-void setup() { 
+void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  float angle = millis() / 1000.0;
-  float value = sin(angle) * 100;
-  Serial.println(value);
+  int data1 = analogRead(A0);
+  int data2 = analogRead(A1);
+  int data3 = analogRead(A2);
+  Serial.print(data1);
+  Serial.print("\t");
+  Serial.print(data2);
+  Serial.print("\t");
+  Serial.println(data3);
   delay(10);
 }
