0f7913a0a73cde4d8154438f7ca9b2c8f9eebc4c
Nurmuhammad <nursherzil@gmail.com>
2026-06-03 14:08:50 +0500

    hello world oled

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define KENGLIK 128
+#define BALANDLIK 64
+#define MANZIL  0x3C
+
+Adafruit_SSD1306 oled(KENGLIK, BALANDLIK, &Wire, -1);
 
 void setup() {
-  Wire.begin();
   Serial.begin(9600);
-  Serial.println("Skanerlash boshlandi...");  
-
-  for(byte addr = 1; addr < 127; addr++) {
-    Wire.beginTransmission(addr);
-    if(Wire.endTransmission() == 0) {
-      Serial.print("topildi: 0x");
-      Serial.println(addr, HEX);
-    }
+  if(!oled.begin(SSD1306_SWITCHCAPVCC, MANZIL)) {
+    Serial.println("OLED TOPILMADI!");
+    while(true);
   }
+
+  oled.clearDisplay(); //ekranni tozalash
+  oled.setTextSize(1); // 1-3
+  oled.setTextColor(WHITE); // rang
+  oled.setCursor(0, 0); // (x, y)
+  oled.println("Hello world!");
+  oled.display(); 
 }
 
 void loop() {
