bf855b444e820f0d89a0c1ec1cbb0cf699bef35f
Nurmuhammad <nursherzil@gmail.com>
2026-06-04 11:46:37 +0500

    ssd1315 buttons

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,40 @@
 #define BALANDLIK 64
 #define MANZIL  0x3C
 
+int buttons[] = {2,3,4,5};
+byte size = sizeof(buttons) / sizeof(buttons[0]);
+byte option;
+
 Adafruit_SSD1306 oled(KENGLIK, BALANDLIK, &Wire, -1);
 
 void setup() {
   Serial.begin(9600);
-  if(!oled.begin(SSD1306_SWITCHCAPVCC, MANZIL)) {
-    Serial.println("OLED TOPILMADI!");
-    while(true);
+  for(byte i = 2; i <= size; i++) {
+    pinMode(i, INPUT_PULLUP);
   }
 
-  oled.clearDisplay(); //ekranni tozalash
-  oled.setTextSize(1); // 1-3
-  oled.setTextColor(WHITE); // rang
-  oled.setCursor(0, 0); // (x, y)
-  oled.println("Hello world!");
-  oled.display(); 
+  if(oled.begin(SSD1306_SWITCHCAPVCC, MANZIL)) {
+    oled.clearDisplay(); //ekranni tozalash
+    oled.setTextSize(1); // 1-3
+    oled.setTextColor(WHITE); // rang
+  }
 }
 
 void loop() {
-
+  if(digitalRead(2) == LOW) {
+    Serial.println("Bosildi pin2");
+    delay(200);
+  }
+  if(digitalRead(3) == LOW) {
+    Serial.println("Bosildi pin3");
+    delay(200);
+  }
+  if(digitalRead(4) == LOW) {
+    Serial.println("Bosildi pin4");
+    delay(200);
+  }
+  if(digitalRead(5) == LOW) {
+    oled.clearDisplay();
+    delay(200);
+  }
 }
