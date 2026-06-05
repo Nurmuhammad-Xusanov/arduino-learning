c1e12e0c8e8e11b533245a074027fbc6ea0a14f4
Nurmuhammad <nursherzil@gmail.com>
2026-06-05 07:01:00 +0500

    ldr

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,18 @@
 #include <Arduino.h>
 
-volatile byte led = 5;
-volatile bool state = false;
-volatile unsigned long lastRun = 0;
-
-unsigned long oldTime = 0;
-const long interval = 1000;
-
-void color_swap() {
-    if(millis() - lastRun > 50) {
-        digitalWrite(led, LOW);
-        led++;
-        if(led == 8) led = 5;
-        lastRun=millis();
-        state = true;
-        digitalWrite(led, state);
-    }
-}
 
 void setup() {
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
-    attachInterrupt(digitalPinToInterrupt(2), color_swap, RISING);
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
 }
 
 void loop() {
-    unsigned long newTime = millis();
-
-    if(newTime - oldTime >= interval) {
-        oldTime = newTime;
-        state = !state;
-        digitalWrite(led, state);
+    int ldr = analogRead(A0);
+    Serial.println(ldr);
+    if (ldr > 350) {
+        digitalWrite(13, HIGH);
+    } else {
+        digitalWrite(13, LOW);
     }
-    
+    delay(100);
 }
