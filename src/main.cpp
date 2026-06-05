e03b7a3e1145e88f4d7cbaa70ceb7365e256dbf9
Nurmuhammad <nursherzil@gmail.com>
2026-06-05 06:29:13 +0500

    fixed bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,20 @@
 #include <Arduino.h>
 
-// Interrupt ichida o'zgaruvchi o'zgaruvchilarga VOLATILE qo'shamiz
 volatile byte led = 5;
+volatile bool state = false;
 volatile unsigned long lastRun = 0;
 
-bool state = false;
 unsigned long oldTime = 0;
 const long interval = 1000;
 
 void color_swap() {
-    // Tugma shovqinini (Bouncing) dasturiy filtrlaymiz
-    if(millis() - lastRun > 200) { // 50ms juda kam, tugma siqilganda 200ms yaxshi ishlaydi
-        
-        digitalWrite(led, LOW); // Yangisiga o'tishdan oldin, hozirgi yonib turgan pinni o'chiramiz!
-        
+    if(millis() - lastRun > 50) {
+        digitalWrite(led, LOW);
         led++;
-        if(led == 8) led = 5; // 5, 6, 7 pinlar aylanadi
-        
-        lastRun = millis();
+        if(led == 8) led = 5;
+        lastRun=millis();
+        state = true;
+        digitalWrite(led, state);
     }
 }
 
@@ -25,11 +22,7 @@ void setup() {
     pinMode(5, OUTPUT);
     pinMode(6, OUTPUT);
     pinMode(7, OUTPUT);
-    
-    // 2-pinni ham INPUT (yoki sizda pull-up bo'lsa INPUT_PULLUP) qilish kerak
-    pinMode(2, INPUT_PULLUP); 
-    
-    attachInterrupt(digitalPinToInterrupt(2), color_swap, FALLING); // INPUT_PULLUP bo'lsa FALLING yaxshi
+    attachInterrupt(digitalPinToInterrupt(2), color_swap, RISING);
 }
 
 void loop() {
@@ -38,6 +31,7 @@ void loop() {
     if(newTime - oldTime >= interval) {
         oldTime = newTime;
         state = !state;
-        digitalWrite(led, state); // Faqat hozirgi tanlangan pin yonib-ochadi
+        digitalWrite(led, state);
     }
+    
 }
