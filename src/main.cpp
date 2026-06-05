d25b7d0ec42e1454c1f9a04c6311aa410996f5ae
Nurmuhammad <nursherzil@gmail.com>
2026-06-05 21:15:03 +0500

    clock

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,52 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <RTClib.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+RTC_DS3231 rtc;
 
-int ping(byte TriggerPin, byte EchoPin) {
-    long duration, distanceCm;
-
-    digitalWrite(TriggerPin, LOW);
-    delayMicroseconds(4);
-
-    digitalWrite(TriggerPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TriggerPin, LOW);
-
-    duration = pulseIn(EchoPin, HIGH);
-
-    distanceCm = duration * 10 / 292 / 2;
-    
-    return distanceCm;
-}
-
+#define KENGLIK 128
+#define BALANDLIK 64
+#define MANZIL 0x3C
+Adafruit_SSD1306 oled(KENGLIK,BALANDLIK, &Wire, -1);
 void setup() {
     Serial.begin(9600);
-    pinMode(4, OUTPUT);
+    if(!rtc.begin()) {
+        Serial.println("RTC modul topilmadi!");
+        while(1);
+    }
+
+    if(rtc.lostPower()) {
+        Serial.println("RTC batareykasi o'chgan");
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
+    if(!oled.begin(SSD1306_SWITCHCAPVCC, MANZIL)) {
+        Serial.println("OLED YOQ NIGER");
+    }
+    oled.clearDisplay();
+    oled.setTextSize(1);
+    oled.setCursor(0,0);
+    oled.setTextColor(WHITE);
 }
 
 void loop() {
-    if(ping(6,5) <= 10) {
-        Serial.println("Close");
-        digitalWrite(4, HIGH);
-        delay(500);
-    } else {
-        Serial.println("Safe");
-        delay(500);
-    }
-    digitalWrite(4, LOW);
+    DateTime now = rtc.now();
+
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+
+    oled.print(now.hour(), DEC);
+    oled.print(":");
+    oled.print(now.minute(), DEC);
+    oled.print(":");
+    oled.print(now.second(), DEC);
+    oled.display();
+    delay(1000);
+    oled.setCursor(0, 0);
+    oled.clearDisplay();
 }
